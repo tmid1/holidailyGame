22ce7f93cecc5a71b4570709a7a5d62eecb6cf16
tmid3 <106893423+tmid1@users.noreply.github.com>
2024-12-25 18:13:44 -0700

    change back

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
-/** @type {import('next').NextConfig} */
+import type { NextConfig } from "next";
+
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true
-  }
+  },
+  distDir: 'dist'
 }
 
 module.exports = nextConfig
+
+export default nextConfig;
